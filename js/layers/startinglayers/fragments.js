ebb697e6ef325f752ca5c549f578e4eab239f957
Waddles <79166144+WaddlesDiscord@users.noreply.github.com>
2021-10-21 14:20:08 +0100

    Update fragments.js

--- js/layers/startinglayers/fragments.js
+++ js/layers/startinglayers/fragments.js
@@ -1,1 +1,25 @@
-
+addLayer("Fragments", {
+    name: "Fragment Combiner",
+    symbol: "FC",
+    position: 0, // Horizontal
+    row: 0,
+    startData() { return {
+        unlocked: true,
+		    points: new Decimal(0),
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "Fragments", // Name of prestige currency
+    baseResource: "Dust", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    layerShown(){return true}
+})
