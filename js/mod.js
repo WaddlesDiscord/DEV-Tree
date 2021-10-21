a8f6e1592dba134cecd538998f1a7f499fc83edb
Waddles <79166144+WaddlesDiscord@users.noreply.github.com>
2021-10-21 13:39:19 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -55,7 +55,7 @@ function addedPlayerData() { return {
 
 // Display extra things at the top of the page
 var displayThings = [
-	'Current endgame: Legit nothing lol'
+	'Current endgame: Legit nothing lol',
 	'This is a WIP'
 ]
 
